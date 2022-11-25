60af9d70c4e265615d296b6ad54ee159b5308131
NarendraTezu <narendra.tezu18@gmail.com>
2022-11-26 01:40:22 +0530

    find positive or negative number

---
+++ positive_negative.py
@@ -0,0 +1,14 @@
+"""
+
+    Write a program to find given number is positive or negative
+
+
+"""
+
+num1 = float(input("Enter your number : "))
+
+if num1 < 0:
+    print("Entered number : ", num1, "is negative number.")
+else:
+    print("Entered number : ", num1, "is positive number.")
+
