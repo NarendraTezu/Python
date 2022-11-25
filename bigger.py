ba6504ce692fac3e7a5f97bc2921020b11612a46
NarendraTezu <narendra.tezu18@gmail.com>
2022-11-26 01:05:56 +0530

    Bigger number program

---
+++ bigger.py
@@ -0,0 +1,15 @@
+"""
+    find bigger number program.
+
+"""
+
+
+num1 = float(input("Enter your num1 : "))
+num2 = float(input("Enter your num2 : "))
+
+if num1 > num2:
+    print("bigger number is : ", num1)
+elif num2 > num1:
+    print("Bigger number is : ", num2)
+else:
+    print("Both are same number  ")
