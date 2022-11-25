638177ff105dc4b7c33c366e10a62501ed02977a
NarendraTezu <narendra.tezu18@gmail.com>
2022-11-26 01:18:47 +0530

    Odd-Even number program

---
+++ odd_even.py
@@ -0,0 +1,11 @@
+"""
+    Write a program for find a is odd or even
+
+"""
+
+num1 = int(input("Enter your number : "))
+
+if num1 % 2 == 0:
+    print("Enter number : ", num1, " is even number.")
+else:
+    print("Enter number : ", num1, " is odd number.")
