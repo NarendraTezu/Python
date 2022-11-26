7ce9b48a255ace60cf86377128620bc27a473889
NarendraTezu <narendra.tezu18@gmail.com>
2022-11-26 11:45:42 +0530

    Sumation of numbers

---
+++ sumation.py
@@ -0,0 +1,11 @@
+"""
+    
+    Write a program to priint addition of 1-100 numbers.
+
+"""
+
+sumation = 0
+
+for i in range(0,100):
+    sumation += i
+print("The sumation of given numbers are : ", sumation)
